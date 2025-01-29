46b36940ae5a8fcb2a4bee7aa681364fc2a385ec
NguyenMinh03 <minhnguyenphuc121103@gmail.com>
2025-01-29 13:29:44 +0200

    Add new utility types and modules for Emotion and MUI; remove unused React DOM static file

--- frontendapp/src/App.tsx
+++ frontendapp/src/App.tsx
@@ -1,15 +1,42 @@
 import YouTubeEmbed from "./YoutubeEmbed";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function App() {
-  // Extract the video ID from the YouTube link
   const videoId = "RBZgpIW08Fk";
-
   return (
-    <div className="App">
-      <h1>FAA camera</h1>
-      <YouTubeEmbed videoId={videoId} />
-    </div>
+    <Router>
+      <div className="App">
+        <nav
+          className="navbar"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Link to="/">Home Page</Link>
+
+          <p>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <Button variant="contained" color="secondary">
+                Log in
+              </Button>
+            </Link>
+          </p>
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>FAA camera</h1>
+                <YouTubeEmbed videoId={videoId} />
+              </>
+            }
+          />
+          <Route path="/login" element={<h1>Log In Page</h1>} />
+          <Route path="/signup" element={<h1>Sign Up Page</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
