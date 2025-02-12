7f93b28e4090d17993ac15e8bce1ff8c2b18006b
NguyenMinh03 <minhnguyenphuc121103@gmail.com>
2025-02-12 10:44:51 +0200

    update failure deploy

--- frontendapp/src/components/pages/Services.tsx
+++ frontendapp/src/components/pages/Services.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Services.css";
-import YouTubeEmbed from "./YouTubeEmbed";
+import YouTubeEmbed from "./YoutubeEmbed";
 
 const Services: React.FC = () => {
   const allEvents = [
