9c455330cb5cbe3e0cec7ba3d35c9ed45c185f41
Polina <polina.domracheva@myy.haaga-helia.fi>
2025-04-02 13:54:17 +0300

    Adding a modal for opening the pictures when clicked

--- frontendapp/src/components/pages/Product/ui/Picture.tsx
+++ frontendapp/src/components/pages/Product/ui/Picture.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Picture.css";
 
 type Props = {
@@ -8,9 +9,29 @@ type Props = {
   };
 };
 
-export const Picture = ({ image }: Props) => (
-  <div className="image-container">
-    <img src={image.url} alt={`Image ${image.id}`} />
-    <p className="timestamp">{image.time}</p>
-  </div>
-);
+export const Picture = ({ image }: Props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <>
+      <div className="image-container" onClick={openModal}>
+        <img src={image.url} alt={`Image ${image.id}`} />
+        <p className="timestamp">{image.time}</p>
+      </div>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close-button" onClick={closeModal}>
+              &times;
+            </button>
+            <img src={image.url} alt={`Image ${image.id}`} className="modal-image" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
